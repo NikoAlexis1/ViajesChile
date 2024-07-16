a8637d5e590c4b7b5fdffa95bebcb250bf890cb2
Nicolas <ncorantehuenchuman@gmail.com>
2024-07-16 18:00:31 -0400

    Modificacion seccion contacto

--- Assets/Js/Script.js
+++ Assets/Js/Script.js
@@ -1,20 +1,25 @@
 import '../../node_modules/jquery/dist/jquery.min.js';
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 
+// Tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-// $(function(){
-//     $(".card-img-top").on('click',function(){
-//         $(".card-body").toggle({
-//             "duration": 700
-//         });
-//     });
-
-//     $(".card-img-top").hover(function(){
-//         $(this).tooltip();
-//     });
-
-//     $("#enviarcontacto").on("click",function() {
-//         alert("El mensaje ya fue enviado correctamente");
-//     });
-
-// });
+// Alert
+let alertContainer = document.getElementById("alertcontainer");
+const appendAlert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+      `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+      `   <div>${message}</div>`,
+      '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      '</div>'
+    ].join('')
+    alertContainer.append(wrapper)
+  }
+  const alertTrigger = document.getElementById('enviarcontacto')
+  if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+      appendAlert('Mesaje enviado correctamente!', 'success')
+    })
+  }
