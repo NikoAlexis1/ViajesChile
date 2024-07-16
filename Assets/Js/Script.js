50d014f1440235ea3e5d013140b50e0e90808a25
Nicolas <ncorantehuenchuman@gmail.com>
2024-07-15 23:18:39 -0400

    modificacion seccion Destacados

--- Assets/Js/Script.js
+++ Assets/Js/Script.js
@@ -2,19 +2,19 @@ import '../../node_modules/jquery/dist/jquery.min.js';
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 
 
-$(function(){
-    $(".card-img-top").on('click',function(){
-        $(".card-body").toggle({
-            "duration": 700
-        });
-    });
+// $(function(){
+//     $(".card-img-top").on('click',function(){
+//         $(".card-body").toggle({
+//             "duration": 700
+//         });
+//     });
 
-    $(".card-img-top").hover(function(){
-        $(this).tooltip();
-    });
+//     $(".card-img-top").hover(function(){
+//         $(this).tooltip();
+//     });
 
-    $("#enviarcontacto").on("click",function() {
-        alert("El mensaje ya fue enviado correctamente");
-    });
+//     $("#enviarcontacto").on("click",function() {
+//         alert("El mensaje ya fue enviado correctamente");
+//     });
 
-});
+// });
